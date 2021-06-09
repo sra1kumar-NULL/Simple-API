649122057250499e7bce96f864d21d6ab43489f7
sra1kumar-NULL <sravansandeep163@gmail.com>
2021-06-09 16:23:50 +0530

    added the greeter api

---
+++ JavaAPI/src/test/java/com/sam/corleone/JavaAPI/JavaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sam.corleone.JavaAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
